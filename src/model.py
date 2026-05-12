f7e5f0486a8cdb1abfd200530130250e2e2e5a46
Jose Villa <josecerna2905@gmail.com>
2026-05-12 08:39:03 -0600

    Added model definition

--- src/model.py
+++ src/model.py
@@ -5,17 +5,27 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, num_classes: int) -> None:
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 32, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+
+        self.dropout = nn.Dropout(0.3)
+
+        self.fc1 = nn.Linear(64 * 5 * 5, 256)
+        self.fc2 = nn.Linear(256, 128) 
+        self.fc3 = nn.Linear(128, num_classes)
+
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+
+        x = x.view(-1, 64 * 5 * 5)
+
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+
         x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+
         return self.fc3(x)
